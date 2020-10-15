a98e2ed148e03d732e1978f841bedbe0d5343847
Sudipta <sudiptabala32@gmail.com>
2020-10-15 07:41:45 +0530

    [Sudipta] Refactor : Method to analyse mood haapy or sad

--- MoodAnalyseTest/UnitTest1.cs
+++ MoodAnalyseTest/UnitTest1.cs
@@ -6,11 +6,12 @@ namespace MoodAnalyseTest
     public class UnitTest1
     {
         [TestMethod]
-        [DataRow(null)]
+        [DataRow("I am in Sad Mood")]
+        [DataRow("I am in Happy Mood")]
         public void TestMethod1(string message)
         {
             //Arrange
-            string expected = "HAPPY";
+            string expected = "SAD";
             MoodAnalyse mood = new MoodAnalyse(message);
             //Act
             var result = mood.analyseMood();
